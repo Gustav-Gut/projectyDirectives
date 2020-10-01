184197e6221aed540797bd00edb2701b36994cb5
Gustavo Gutierrez <gustavo.gutierrezy@gmail.com>
2020-10-01 18:39:28 -0300

    [ADD] ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public available: boolean = true;
+
   title = 'projectDirectives';
 }
